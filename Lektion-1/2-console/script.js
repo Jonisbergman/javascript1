9ded0760389e480a2e20f32027aa3001bae42ccd
Jonis Bergman <jonis.bergman@utb.ecutbildning.se>
2021-12-06 10:01:04 +0100

    Hello World

---
+++ Lektion-1/2-console/script.js
@@ -0,0 +1,28 @@
+//  En kommentar på en rad (Ctrl + *)
+
+/* 
+en kommentar kan skrivas såhär
+om det är flera rader (shift + alt + a)
+*/
+
+// skriver ut någonting i konsolen
+console.log('Här skriver jag ut någonting i konsolen');
+console.warn('Detta är en varning');
+console.error('Detta är en varning');
+
+// console.lg('hej')
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text';
+
+// Högnivå språk (mellan användaren och systemet)
+//--------------------------------------------------
+// (Javascript)
+// Python
+// C# / Java
+
+//Lågnivå språk (mellan systemet och hårdvara)
+// -------------------------------------------------
+// C++
+// C
+// Mikrokod
